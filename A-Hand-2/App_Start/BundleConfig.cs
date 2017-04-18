a673359249c4df83d3f75cd14d2510fa16419bd7
Incub8it-Des <desp@incub8it.org>
2017-04-18 22:36:12 +0200

    Updated CSS to Lumen

--- A-Hand-2/App_Start/BundleConfig.cs
+++ A-Hand-2/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace A_Hand_2
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
