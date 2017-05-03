4ee5cd22eac3036791597cc941853d50b3d6c7e0
Incub8it-Des <desp@incub8it.org>
2017-05-03 16:17:55 +0200

    Reenabled AskTaskController and AskTaskViewModel

--- A-Hand-2/ViewModels/AskTaskViewModel.cs
+++ A-Hand-2/ViewModels/AskTaskViewModel.cs
@@ -8,19 +8,19 @@ namespace A_Hand_2.ViewModels
 {
     public class AskTaskViewModel
     {
-        //public IEnumerable<TaskType> TaskTypes { get; set; }
-        //public AskTask AskTask {get; set;}
-        
+        public IEnumerable<TaskType> TaskTypes { get; set; }
+        public AskTask AskTask { get; set; }
+
     }
 
     public class CascadeViewModel
     {
         public IEnumerable<ApplicationUser> User { get; set; }
-        //public IEnumerable<Customer> Customers { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
     }
 
     public class CustomerViewModel
     {
-        //public IEnumerable<Customer> Customers { get; set; }
+        public IEnumerable<Customer> Customers { get; set; }
     }
 }
