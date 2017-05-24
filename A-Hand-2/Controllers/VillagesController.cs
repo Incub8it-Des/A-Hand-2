08890450cc44c2c385098d292f559e9e6e9995b6
Incub8it-Des <desp@incub8it.org>
2017-05-24 10:00:34 +0200

    Village Controller and views created

---
+++ A-Hand-2/Controllers/VillagesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using A_Hand_2.Models;
+
+namespace A_Hand_2.Controllers
+{
+    public class VillagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Villages
+        public ActionResult Index()
+        {
+            var villages = db.Villages.ToList();
+            villages.OrderBy(v => v.VillageName);
+            return View(villages);
+        }
+
+        // GET: Villages/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Village village = db.Villages.Find(id);
+            if (village == null)
+            {
+                return HttpNotFound();
+            }
+            return View(village);
+        }
+
+        // GET: Villages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Villages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,VillageName")] Village village)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Villages.Add(village);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(village);
+        }
+
+        // GET: Villages/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Village village = db.Villages.Find(id);
+            if (village == null)
+            {
+                return HttpNotFound();
+            }
+            return View(village);
+        }
+
+        // POST: Villages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,VillageName")] Village village)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(village).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(village);
+        }
+
+        // GET: Villages/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Village village = db.Villages.Find(id);
+            if (village == null)
+            {
+                return HttpNotFound();
+            }
+            return View(village);
+        }
+
+        // POST: Villages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Village village = db.Villages.Find(id);
+            db.Villages.Remove(village);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ A-Hand-2/Migrations/201705240748379_2ndSeedVillages.cs
@@ -0,0 +1,115 @@
+namespace A_Hand_2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _2ndSeedVillages : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Newport')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Brading')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Cowes')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'East Cowes')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Ryde')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Sandown')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Shanklin')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Ventnor')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Yarmouth')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Adgestone')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Afton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Afton Park')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Alum Bay')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Alverstone Garden Village')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Appley')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Apse Heath')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Ashey')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Barton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Bathingbourne')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Bierley')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Binfield')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Bouldnor')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Borthwood')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Bowcombe')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Branstone')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Chale Green')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Compton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Cross Lane')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Culver Down')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Downend')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Easton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Egypt Point')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Elmfield')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Five Houses')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Foreland Fields')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Forest Side')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Freshwater Bay')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Great Thorness')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Gunville')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Hale Common')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Hamstead')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Havenstreet')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Haylands')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Hillway')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Horringford')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Hulverstone')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Hunnyhill')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Kings Quay')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Kingston')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Knighton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Lane End')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Limerstone')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Little Atherfield')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Little Thorness')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Littletown')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Locksgreen')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Lowtherville')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Marks Corner')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Mersley')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Moortown')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Morton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Mottistone')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Nettlecombe')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Ninham')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Niton Undercliff')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Norton')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Norton Green')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Nunwell')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Oakfield')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Osborne')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Parkhurst')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Pondwell')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Princelett')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Puckaster')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Quarr Hill')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Queens Bower')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Rew Street')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Rookley Green')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Roud')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Rowridge')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Sandford')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Seagrove Bay')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Shalcombe')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Shide')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Staplers')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Steephill')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Steyne Cross')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Thorley')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Thorley Street')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Thorncross')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Totland Bay')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Upper Ventnor')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Wellow')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Whiteley Bank')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Winford')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Wootton Common')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Yafford')");
+            Sql("INSERT INTO [dbo].[Villages] ([VillageName]) VALUES (N'Yarbridge')");
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
